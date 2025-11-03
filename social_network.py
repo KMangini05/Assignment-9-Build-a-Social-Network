2e88dbd41a9d5544131dc3f8be04a6fcc5583fdb
KMangini05 <kmangini22@arizonachristian.edu>
2025-11-02 20:08:11 -0700

    Add files via upload

--- social_network.py
+++ social_network.py
@@ -8,8 +8,13 @@ class Person:
         add_friend(friend): Adds a friend to the person's friend list.
    '''
     
-    pass # Delete this line to implement the Person class
+    def __init__(self, name):
+        self.name = name
+        self.friends = []
 
+    def add_friend(self, friend):
+        if friend not in self.friends:
+            self.friends.append(friend)
 
 class SocialNetwork:
     '''
@@ -22,8 +27,41 @@ class SocialNetwork:
         print_network(): Prints the names of all people and their friends.
     '''
     
-    pass # Delete this line to implement the SocialNetwork class
+    def __init__(self):
+        self.people = {}
 
+    def add_person(self, name):
+        if name not in self.people:
+            self.people[name] = Person(name)
+        else:
+            print(f"{name} already exists in the network.")
+
+    def add_friendship(self, person1_name, person2_name):
+        if person1_name not in self.people or person2_name not in self.people:
+            print("Error: One or both people do not exist in the network.")
+            return
+        
+        person1 = self.people[person1_name]
+        person2 = self.people[person2_name]
+
+        person1.add_friend(person2)
+        person2.add_friend(person1)
+
+    def print_network(self):
+        for name, person in self.people.items():
+            friends_names = [friend.name for friend in person.friends]
+            print(f"{name}: {', '.join(friends_names)}" if friends_names else f"{name}: No firends yet")
 
 
 # Test your code here
+network = SocialNetwork()
+
+network.add_person("Alex")
+network.add_person("Jordan")
+network.add_person("Morgan")
+
+network.add_friendship("Alex", "Jordan")
+network.add_friendship("Alex", "Morgan")
+network.add_friendship("Jordan", "Taylor")
+
+network.print_network()
